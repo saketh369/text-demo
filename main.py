2d9624378e155b7999f4b48dd9d4fc926208acfb
Saketh Gowerneni <sakethgowerneni@macbookpro.mynetworksettings.com>
2024-09-30 12:58:09 -0400

    first program

---
+++ main.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the two numbers
+sum = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is: {sum}")
